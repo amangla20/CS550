5335603b4fe8e80d951343ea7b4240d496d1a0f4
Anjali <amangla20@choate.edu>
2018-09-23 18:00:52 -0400

    saving progress of story so far

--- adventuregame.py
+++ adventuregame.py
@@ -6,9 +6,9 @@ startTime = None
 def start():
 	global startTime
 	startTime = time.time()
-	print("Welcome to Text Adventures! In this game, you will be taken through a series of steps in order to restore order in the kingdom. Be careful what you choose, as some paths may take longer than others. Be thorough, but not too thorough in your search. Remember, all choices you make in this quest will require an answer of 1 or 2, so make sure to remember to use these answer choices only. ")
+	print("Welcome to Text Adventures! In this game, you will be taken through a series of steps in order to restore order in the kingdom. Be careful what you choose, as some paths may take longer than others. For picking the wrong path, you may be penalized with time deduction. Be thorough, but not too thorough in your search. Remember, the clock is ticking! All choices you make in this quest will require an answer of 1 or 2, so make sure to remember to use these answer choices only.")
 
-	result = input("\nYou are here to save the prince!\n\nHe has been kidnapped by the evil sorceress that curses our kingdom. First, you must look for clues as to where the sorceress could have taken him.\n\nYou can either: \n1) Visit the throne room or \n2) Visit the stables.\n\n>>")
+	result = input("\n\n\nYou are here to save the prince!\n\nHe has been kidnapped by the evil sorceress that curses our kingdom. First, you must look for clues as to where the sorceress could have taken him.\n\nYou can either: \n1) Visit the throne room or \n2) Visit the stables.\n\n>> ")
 
 """
 	# one way to do it
@@ -30,7 +30,7 @@ def checkTime():
 	if (time.time() < startTime + 300):
 		pass
 	else:
-		timedout = input("Oh no! You took too much time to save the prince, and now the sorceress has whisked him away to a world where he could never be found. You've failed your quest! The kingdom will now fall into eternal mourning, the curse set upon the kingdom by the sorceress. Would you like to try to save the prince again? ")
+		timedout = input("\n\nOh no! You took too much time to save the prince, and now the sorceress has whisked him away to a world where he could never be found. You've failed your quest! The kingdom will now fall into eternal mourning, the curse set upon the kingdom by the sorceress. Would you like to try to save the prince again? ")
 		if checkInput(timedout, 'N', 'Y') == 'N':
 			print("Oh well. The kingdom will remain desolate until the next hero comes along. Thanks for trying!")
 		elif checkInput(timedout, 'N', 'Y') == 'Y':
@@ -43,32 +43,61 @@ def checkInput(choice, a, b):
 
 def throneRoom():
 	checkTime()
-	king = input("\n\nThe king is in much distress, but they say he was the last one to see his son before the abduction.\n\nDo you \n1) ask him for clues or \n2) let him grieve in peace?\n\n>>")
+	king = input("\n\nThe king is in much distress, but they say he was the last one to see his son before the abduction.\n\nDo you \n1) ask him for clues or \n2) let him grieve in peace and head to the stables?\n\n>> ")
 
 	if checkInput(king, '1', '2') == '1':
 		pass
 		kingClue() # have the clue lead them to somewhere that would lead them to where the stables would lead them
 	elif checkInput(king, '1', '2') == '2':
 		pass
-		peaceGrieve() # have them go now to the stables
+		peaceGrieve() # have them go now to the queen
 
 def stables():
 	checkTime()
-	stableBoy = input("\n\nThe stable boy is looking for the horse, Misty, that the prince was riding when he was abducted. However, he forgot if Misty likes 1) carrots or 2) peanuts. What will you use to lure Misty? ")
+	stableBoy = input("\n\nThe stable boy is looking for the horse, Misty, that the prince was riding when he was abducted. Misty was last seen at the garden with the prince, who was tending to his favorite sunflower patch. The stable boy seems very sad, as Misty was his favorite horse to take care of. Misty always 'knew the right way to go about things.' Would you like to 1) go to the garden or 2) help the stable boy find Misty? \n\n>> ")
 	if checkInput(stableBoy, '1', '2') == '1':
-		pass
+		garden()
 	elif checkInput(stableBoy, '1', '2') = '2':
-		pass
+		findMisty()
+
+def findMisty():
+	checkTime()
+	startTime += 30
+	print("\n\nYou spend an hour looking for Misty, while time to save the prince ticks by. Then, a thought occurs to you. The prince could have been abducted along with Misty! After all, the sorceress could have somehow led them through a spell to her lair. You decide that you must go back to the garden and follow the stable boy's original clue.\n\n>>")
+	garden()
 
 def kingClue():
 	checkTime()
-	clue = input("\n\nMy boy, oh my boy! What will we ever do? He was my pride and joy, that boy. He was so found of that garden of ours, that he spent every waking minute in it tending to the sunflowers. That wretched wench dragged him off because she was jealous of my happiness.\n\nNow that you have gained this clue, do you wish to 1) go straight to the garden or 2) talk to the stable boy to gain more clues?\n\n>>")
+	clue = input("\n\nMy boy, oh my boy! What will we ever do? He was my pride and joy, that boy. He was so found of that garden of ours, that he spent every waking minute in it tending to the sunflowers. That wretched wench dragged him off because she was jealous of my happiness. Just remember one thing! The sorceress once told me that 'Right is the way of evil.'\n\nNow that you have gained this clue, do you wish to 1) go straight to the garden or 2) talk to the stable boy to gain more clues?\n\n>> ")
 	if checkInput(clue, '1', '2') == '1':
-		pass
+		garden()
 	if checkInput(clue, '1', '2') == '2':
 		stables()
 
 def peaceGrieve():
-	pass
+	checkTime()
+	queen = input("\n\nNow that you left the king to grieve in peace, you encounter the queen walking back from her morning walk around the gardens. She tells you that her husband would answer any questions asked to him in this period of grieving because he is determined to find his son and bring him back. Do you 1) go back to the throne room to talk to the king or 2) head to the stables?\n\n>> ")
+
+def garden():
+	checkTime()
+	garden = input("\n\nNow that you have found the sunflowers, you notice something interesting about the sunflowers closest to the forest. It looks like they were trampled by something somehow, maybe a horse. It looks as though the prince went this way! There are two paths into the forest. Do you go to the\n1) left or the\n2) right? ")
+	if checkInput(garden, '1', '2') == '1':
+		leftPath()
+	elif checkInput(garden, '1', '2') == '2':
+		rightPath()
+
+def leftPath():
+	checkTime()
+	leftChoice = input("\n\nThe path ahead is windy and filled with chilly winds. Knowing that the sorceress has a knack for picking scary places to set up her lair, you are reassured by the state of this path leading you to your destiny. Do you want to\n1) continue down the left path or\n2) turn back to choose between paths again? ")
+	if checkInput(leftChoice, '1', '2') == '1':
+		continueLeft()
+	elif checkInput(leftChoice, '1', '2') == '1':
+		garden()
+
+def continueLeft():
+	checkTime()
+	startTime += 15
+	goBack = input("\n\nYou walked for quite a while and have now come to a dead end in the road. Looks like you didn't heed the advice you were given very well! ")
+
 
 start()
