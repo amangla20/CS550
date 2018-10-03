94e7650fafd1a23a85c78cd88b69b256288c267d
Anjali <amangla20@choate.edu>
2018-10-03 09:12:03 -0400

    class work

--- mine_sweeper.py
+++ mine_sweeper.py
@@ -1,3 +1,7 @@
+# This is the link on string splitting:
+# https://www.geeksforgeeks.org/python-string-split/
+
+
 # I spent an hour and really couldn't figure out how to count the spaces next to the bomb and show the number. This is my try.
 # the grid starts with (0,0) at the top left corner, and with the first row being 0 value for (x,y) (sort of like the axis if it was a coordinate grid)
 
@@ -17,8 +21,7 @@ field = [[0]*w for x in range(h)]
 #for x in range(len(field)):
 	#print(*field[x])
 
-# I found that randint was better than randrange because it includes the last number in it: https://stackoverflow.com/questions/3540431/difference-between-random-randint-vs-randrange
-
+# MAKE AN ARRAY OF THE X AND Y VALUES OF THE BOMBS
 for number in range(b):
 	x = r.randrange(1,w-1)
 	y = r.randrange(1,h-1)
@@ -57,36 +60,42 @@ for y in range(1,h-1):
 # print at the end with 0s turning into the right number
 # is there a way to "hide" a board or solution?
 # this is my "hidden solution" field
-"""
+
 for y in range(1,h-1):
 	for x in range(1,w-1):
 		print(field[y][x],end=" ")
 	print("")
-"""
+
 # use "X" or 0?
-userField = [["X"]*w for x in range(h)]
-for y in range(1,h-1):
-	for x in range(1,w-1):
-		print(userField[y][x],end=" ")
-	print("")
+def printUserField():
+	userField = [["X"]*w for x in range(h)]
+	for y in range(1,h-1):
+		for x in range(1,w-1):
+			print(userField[y][x],end=" ")
+		print("")
 
 def choose():
-	space = input("Choose a space to either clear or flag. Provide the x and y coordinates, and type 'f' for flag and 'c' for clear.") # can command line arguments work outside first python file calling
+	space = input("Choose a space to either clear or flag. Provide the x and y coordinates, and type 'f' for flag and 'c' for clear. Enter your answer in the following format: x, y, [f or c]") # can command line arguments work outside first python file calling
 	# if flag, else (else if clear)
-	if space[2] == f:
-		userField[space[1]][space[0]] == "f"
+	userChoice = space.split(', ')
+	y = userChoice[1]
+	x = userChoice[0]
+	if userChoice[2] == "f":
+		userField[y][x] == "f"
 		flagCount += 1
+		checkFlags()
+		printUserField()
 		choose()
 	else:
 		# check using solution
-		if field[space[1]][space[0]] == "*":
+		if field[y][x] == "*":
 			gameOver()
 		else:
-			if field[space[1]][space[0]] == 0:
+			if field[y][x] == 0:
 				# reveal contiguous spaces
 				choose()
 			else:
-				userField[space[1]][space[0]] = field[space[1]][space[0]]
+				userField[y][x] = field[y][x]
 				choose()
 
 # while loop saying while whole board is not filled, do this action, and when it is full, check to see if the flags are over bombs
@@ -94,4 +103,14 @@ def choose():
 def gameOver():
 	print("Sorry, you just unearthed a bomb!")
 
+def checkFlags():
+	if flagCount == b:
+		# wrong this is the if statement to use for ending game checking all clear spaces and flags
+		if userField[y][x] == field[y][x]:
+			print("You won!")
 
+
+		# check if flags are in the right spaces
+
+printUserField()
+choose()
