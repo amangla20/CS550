be10db901e4a1ff21a1779c404b0dde62cc7c954
Anjali <amangla20@choate.edu>
2018-11-07 09:24:50 -0500

    have things to work on but main function is 90% there

--- elements.py
+++ elements.py
@@ -1,11 +1,31 @@
 # Compose  a  data  type  Element  for  entries  in  the  periodic  table  of  elements.  Include  data  type  values  for  element,  atomic  number,  symbol,  and  atomic  weight  and  accessor  methods  for  each  of  these  values.  Then,  create  a  data  type  PeriodicTable  that  reads  values  from  a  file  to  create  a  list  of  Element  objects  and  responds  to  queries  on  input  so  that  a  user  can  type  a  molecular  equation  like  H2O  and  the  program  responds  by  printing  the  molecular  weight.  What  other  kinds  of  interactions  can  you  create  within  this  class?  The  file  here:  https://introcs.cs.princeton.edu/python/32class/elements.csv    contains  the  data  that  the  program  should  read.  Include  fields  for  element,  atomic  number,  symbol,  and  atomic  weight.  (Ignore  fields  for  boiling  
 # point,  melting  point,  density  (kg/m3),  heat  vapour  (kJ/mol),  heat  fusion  (kJ/mol),  thermal  conductivity  (W/m/K),  and  specific  heat  capacity  (J/kg/K)  since  it's  not  known  for  all  elements).  The  file  is  in  CSV  format  (fields  separated  by  commas).  From:  https://introcs.cs.princeton.edu/python/32class/
 class Element:
+	#init function specifies all of our arguments
 	def __init__(self, element, number, symbol, weight):
 		self.element = element
 		self.number = number
 		self.symbol = symbol
 		self.weight = weight
+
+	#accessor methods
+	def getElement(self):
+		return self.element
+	def setElement(self,element):
+		self.element = element
+	def getNumber(self):
+		return self.number
+	def setNumber(self):
+		self.number = number
+	def getSymbol(self):
+		return self.symbol
+	def setSymbol(self):
+		self.symbol = symbol
+	def getWeight(self):
+		return self.weight
+	def setWeight(self):
+		self.weight = weight
+
 
 	def __str__(self):
 		info = "Element name: " + self.element
--- periodic_table.py
+++ periodic_table.py
@@ -1,4 +1,6 @@
-# https://realpython.com/python-csv/
+# Things to work on:
+# The result of HE2 (need to make else statement?)
+# The molecular weight if there are two or more digits
 import csv
 from elements import Element
 import re
@@ -10,7 +12,7 @@ class PeriodicTable:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
                 self.elements.append(Element(row[0], row[1], row[2], row[3]))
-                # print(f'\t{row[0]} has an atomic number of {row[1]}. Its periodic symbol is {row[2]}, and its atomic weight is {row[3]} g.')
+      
     def __str__(self):
         result = ""
         for i in self.elements:
@@ -19,7 +21,7 @@ class PeriodicTable:
 
     def elchoice(self, elchoice):
         for elementdata in self.elements:
-            if elementdata.element.upper() == elchoice.upper():
+            if elementdata.getElement().upper() == elchoice.upper():
                 return elementdata
         noelement = "There is no element by that name."
         return noelement
@@ -28,32 +30,51 @@ class PeriodicTable:
         # be able to divide the molecular formula into elements and add their weights by that and multiplying by the number after it
         multiplier = 1
         formula_weight = 0
+        result = ""
         split_formula = re.findall('[A-Z][^A-Z]*', formula)
+        result = "\nThat is not a valid molecular formula."
         for i in range(len(split_formula)):
             for elementdata in self.elements:
+                pos = 0
                 for letter in split_formula[i]:
                     if letter.isdigit():
-                        multiplier = int(letter)
+                        # AM: Still not working for two-digit numbers
+                        multiplier = int(split_formula[i][pos:])
+                        print(letter,pos, multiplier)
                         split_formula[i] = split_formula[i].replace(letter, "")
+                        break
+                    pos += 1
                         #split_formula[i].remove(letter)
+                result = "\nThat is not a valid molecular formula."
                 if elementdata.symbol == split_formula[i]:
-                    formula_weight += float(elementdata.weight)*multiplier
+                    formula_weight += float(elementdata.getWeight())*multiplier
                     multiplier = 1
+                    result = str(formula_weight)
+                    if i == len(split_formula)-1:
+                        return result
         # for x in split_formula:
         #     for letter in x:
         #         if letter.isdigit():
         #             formula_weight*=int(letter)
-        return formula_weight
+        return result
 
 
+def main():
+    print("Welcome to the Periodic Table Mastery Chart! This program is designed to help the user with chemistry homework and become well-equipped with the elements.\n")
+    while True:
+        table1 = PeriodicTable()
 
-table1 = PeriodicTable()
-# print(str(table1))
+        actionchoice = input("Would you like to...\n1. Get an element's data\n2. Get a molecular formula's weight?\n3. Exit this program.\n>> ")
+        if actionchoice == '1':
+            elchoice = input("Enter an element name to get its data: ")
+            print(table1.elchoice(elchoice))
+        elif actionchoice == '2':
+            formula = input("Enter a molecular formula to get its weight: ")
+            print(table1.weight(formula))
+        elif actionchoice == '3':
+            print("Goodbye!")
+            break
+        else:
+            print("That's not an option. Please enter a 1, 2, or 3.")
 
-
-print("Welcome to the Periodic Table Mastery Chart! This program is designed to help the user with chemistry homework and become well-equipped with the elements.\n")
-elchoice = input("Enter an element name to get its data: ")
-print(table1.elchoice(elchoice))
-formula = input("Enter a molecular formula to get its weight: ")
-print(table1.weight(formula))
-
+main()
