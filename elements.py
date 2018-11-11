64f5fd9052fbd2ce335620367be6cc6376727ac0
Anjali <amangla20@choate.edu>
2018-11-11 18:33:13 -0500

    final edits with commenting

--- elements.py
+++ elements.py
@@ -1,14 +1,21 @@
-# Compose  a  data  type  Element  for  entries  in  the  periodic  table  of  elements.  Include  data  type  values  for  element,  atomic  number,  symbol,  and  atomic  weight  and  accessor  methods  for  each  of  these  values.  Then,  create  a  data  type  PeriodicTable  that  reads  values  from  a  file  to  create  a  list  of  Element  objects  and  responds  to  queries  on  input  so  that  a  user  can  type  a  molecular  equation  like  H2O  and  the  program  responds  by  printing  the  molecular  weight.  What  other  kinds  of  interactions  can  you  create  within  this  class?  The  file  here:  https://introcs.cs.princeton.edu/python/32class/elements.csv    contains  the  data  that  the  program  should  read.  Include  fields  for  element,  atomic  number,  symbol,  and  atomic  weight.  (Ignore  fields  for  boiling  
-# point,  melting  point,  density  (kg/m3),  heat  vapour  (kJ/mol),  heat  fusion  (kJ/mol),  thermal  conductivity  (W/m/K),  and  specific  heat  capacity  (J/kg/K)  since  it's  not  known  for  all  elements).  The  file  is  in  CSV  format  (fields  separated  by  commas).  From:  https://introcs.cs.princeton.edu/python/32class/
+# Sonali Singh and Anjali Mangla
+# November 12, 2018
+# Source for data: https://introcs.cs.princeton.edu/python/32class/
+# Source for coloring text: http://ozzmaker.com/add-colour-to-text-in-python/
+# OMH: I have neither given nor received any unauthorized aid.
+
+# creates element object with all of its data
 class Element:
-	#init function specifies all of our arguments
+
+	# init function initializes all element characteristics
 	def __init__(self, element, number, symbol, weight):
 		self.element = element
 		self.number = number
 		self.symbol = symbol
 		self.weight = weight
 
-	#accessor methods
+	# accessor methods
+	# useful for privacy
 	def getElement(self):
 		return self.element
 	def setElement(self,element):
@@ -26,14 +33,13 @@ class Element:
 	def setWeight(self):
 		self.weight = weight
 
-
+	# prints "pretty"
 	def __str__(self):
-		info = "Element name: " + self.element
-		info += "\nAtomic number: " + str(self.number)
-		info += "\nSymbol: " + self.symbol
-		info += "\nAtomic weight: " + str(self.weight) + " g"
+
+		# info string contains all of element's data with labels in front
+		# used ansi escape codes to change the colors of the printed information
+		info = "\033[1;31;48m\nElement name: \033[0;30;48m" + self.element
+		info += "\033[1;31;48m \nAtomic number: \033[0;30;48m" + str(self.number)
+		info += "\033[1;31;48m \nSymbol: \033[0;30;48m" + self.symbol
+		info += "\033[1;31;48m \nAtomic weight: \033[0;30;48m " + str(self.weight) + " g/mol"
 		return info
-
-
-# element1 = Element("Hydrogen", 1, "H", 1.01)
-# print(element1)
--- periodic_table.py
+++ periodic_table.py
@@ -1,14 +1,23 @@
-# Things to work on:
-# comment nicely
-import csv
+# Sonali Singh and Anjali Mangla
+# November 12, 2018
+# Sources: spread out throughout code on top of functions that use them.
+# Source to add colors to text: http://ozzmaker.com/add-colour-to-text-in-python/
+# Source for element data: https://introcs.cs.princeton.edu/python/32class/
+# OMH: I have neither given nor received any unauthorized aid.
+
+# importing element class from separate file
 from elements import Element
 import re
-from PIL import Image
+import csv
 
+# this class forms a list with all of the elements in the periodic table
+# can be called upon to access information for different elements and formulas.
+# reads stored data from a csv file.
 class PeriodicTable:
-    # set up elements in PT
-    # read in from file using whatever library
-    # stored as list of element objects
+
+    # sets up elements in PT
+    # reads in from 'elements' file using Python's CSV module
+    # stored as list of element objects in self.elements list
     # Source to learn CSV reading in python: https://realpython.com/python-csv/
     def __init__(self):
         self.elements = []
@@ -16,19 +25,28 @@ class PeriodicTable:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
                 self.elements.append(Element(row[0], row[1], row[2], row[3]))
-      
+    
+    # prints out all of the data for specified element(s) in the list in a readable fashion
     def __str__(self):
         result = ""
+
+        # each data for element(s) in the list (ex; name, atomic number, etc.) is separated into a diff. line for clarity
         for i in self.elements:
             result += str(i) + "\n"
         return result
 
+    # elchoice function returns all of the data for an element
     def elchoice(self, elchoice):
+
+        # looks through periodic table list for an element name or symbol that matches the user input
         for elementdata in self.elements:
+            
+            # if found, returns the data for that element
             if elementdata.getElement().upper() == elchoice.upper() or elementdata.getSymbol().upper() == elchoice.upper():
                 return elementdata
 
     # This function calculates the molecular weight in g/mol of any molecular formula that the user puts in by splitting the molecular formula into strings by capital letters, so if the symbol has two letters or one it will be separated, and includes numbers in these separate elements of the list. It then checks which character in the formula is a digit in order to make that the multiplier against the weight of the element in each part of the list.
+
     # Source to split a string at capital letters: https://stackoverflow.com/questions/2277352/split-a-string-at-uppercase-letters
 
     def weight(self, formula):
@@ -42,7 +60,7 @@ class PeriodicTable:
         # split the formula input by finding capital letters and splitting at those points
         split_formula = re.findall('[A-Z][^A-Z]*', formula)
         # default print of result
-        result = "\nThat is not a valid element or formula."
+        result = "\nThat is not a valid entry. Check capitalization and spelling"
 
         # loop through split_formula list
         for i in range(len(split_formula)):
@@ -69,14 +87,17 @@ class PeriodicTable:
                     # set multiplier back to 1
                     multiplier = 1
                     # add onto the result, rounding the weight
-                    result = "Weight: " + str(round(formula_weight , 2)) + " g/mol"
+                    result = "\033[1;31;48m\nMolecular Weight: \033[0;30;48m" + str(round(formula_weight , 2)) + " g/mol"
                     # return the result when you have gone through all of split_formula
                     if i == len(split_formula)-1:
                         return result
         return result
 
-    #recognizes whether the user input is an element or a molecular formula, and sets the 'identify' variable accordingly
+    # recognizes whether the user input is an element or a molecular formula, and sets the 'identify' variable accordingly
     def recognize(self, actionchoice):
+
+        # looks through periodic table list for matching element name or symbol
+        # identify value is True for an element and False for a formula
         for item in self.elements:
             if item.getElement().upper() == actionchoice.upper() or item.getSymbol().upper() == actionchoice.upper(): 
                 identify = True
@@ -84,3 +105,6 @@ class PeriodicTable:
             else:
                 identify = False
         return identify
+
+# main function with action lines is in separate file
+
