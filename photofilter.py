cf09e92fb246037d95ae55a83c3c35399254e737
Anjali <amangla20@choate.edu>
2018-11-16 11:41:32 -0500

    final filter

--- photofilter.py
+++ photofilter.py
@@ -2,8 +2,9 @@ from PIL import Image
 import math
 import colorsys
 
-path = "healey_1.jpeg" # Your image path 
-img = Image.open(path)
+path1 = input("What is the name of the image you would like to filter? ")
+# path = "healey_1.jpeg" # Your image path 
+img = Image.open(path1)
 imgx, imgy = img.size
 newimg = Image.new("RGB", (imgx, imgy))
 for x in range(1, imgx-1):  # ignore the edge pixels for simplicity (1 to width-1)
@@ -20,32 +21,50 @@ for x in range(1, imgx-1):  # ignore the edge pixels for simplicity (1 to width-
         b = p[2]
 
         r = y/64
-        g = (g/500)**2
+        g = (g/500)
         b = (b/300)
 
         r,g,b = colorsys.hsv_to_rgb(r,g,b)
 
         newimg.putpixel((x,y),((int(r*256)),(int(g*256)),(int(b*256))))
-newimg.show()
-newimg.save("filteredphoto.png", "PNG")
-# path = "filteredphoto.png"
-# img1 = Image.open(path)
-# imgx, imgy = img.size
-# img2 = Image.new("RGB", (imgx, imgy))
+#newimg.show()
+newimg.save("horizontallines.png", "PNG")
+path2 = "horizontallines.png"
+img1 = Image.open(path2)
+imgx, imgy = img.size
+img2 = Image.new("RGB", (imgx, imgy))
 
-# for x in range(1,imgx-1):
-# 	for y in range(1, imgy-1):
-# 		p = img.getpixel((x,y))
-# 		r = p[0]
-# 		g = p[1]
-# 		b = p[2]
+for y in range(1, imgy-1):
+	for x in range(1, imgx-1):
+		p = img.getpixel((x,y))
+		r = p[0]
+		g = p[1]
+		b = p[2]
 
-#         r = x/64
-#         g = (g/500)**2
-#         b = (b/300)
+		r = x/64
+		g = (g/500)
+		b = (b/300)
 
-#         r,g,b = colorsys.hsv_to_rgb(r,g,b)
+		r,g,b = colorsys.hsv_to_rgb(r,g,b)
 
-#         img2.putpixel((x,y),((int(r*256)),(int(g*256)),(int(b*256))))
+		img2.putpixel((x,y),(int(r*256),int(g*256),int(b*256)))
 
-# img2.show()
+#img2.show()
+img2.save("verticallines.png", "PNG")
+
+path3 = "verticallines.png"
+# Take two images for blending them together   
+image1 = Image.open(path2)
+image2 = Image.open(path3)
+
+# Make sure images got an alpha channel
+image3 = image1.convert("RGBA")
+image4 = image2.convert("RGBA")
+
+# alpha-blend the images with varying values of alpha
+alphaBlended = Image.blend(image3, image4, alpha=.6)
+
+# Display the alpha-blended images
+alphaBlended.show()
+
+alphaBlended.save("filteredphoto.png")
