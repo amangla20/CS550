6ed2b1cb1b5d9ba9c30a930289eab01e947e4879
Anjali <amangla20@choate.edu>
2019-02-13 08:03:17 -0500

    so far

--- worlds_hardest_gamefinal.py
+++ worlds_hardest_gamefinal.py
@@ -2,16 +2,28 @@ import pygame
 import math
 
 pygame.init()
-size = [500, 500]
+height = 500
+width = 500
+size = [width, height]
+balls = []
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("World's Hardest Game! Can you beat it in time?")
 done = False
+def oscillate_vertical(posx, posy):
+	for i in range(height - posy):
+		ball = Ball(posx, posy)
+		posy += 1
 class Ball:
 	def __init__(self, posx, posy):
 		self.posx = posx
 		self.posy = posy
-		shape = pygame.draw.circle(screen, (0, 0, 255), [posx, posy], 50)
-		return shape
+		pygame.draw.circle(screen, (0, 0, 255), [posx, posy], 5)
+
+	def oscillate_vertical(self, posy):
+		for i in range(height - posy):
+			self.posy = posy
+			posy += 1
+			return posy
 
 while not done:
 	# clock.tick(10)
@@ -19,7 +31,20 @@ while not done:
 		if event.type == pygame.QUIT:
 			done = True
 	pygame.display.flip()
-	ball1 = Ball(100, 100)
+	#ball1 = Ball(100, 100)
+	posx = int(width/10)
+	posy = int(height/5)
+	for i in range(5):
+		balls.append(posx)
+		posx += 100
+
+	for i in range(5):
+		oscillate_vertical(balls[i], posy)
+
+
+
+
+	pygame.display.flip()
 pygame.quit()
 # hints with text box for each level 
 
