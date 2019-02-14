b7284ad7801ddd1dbfa74a850135873b98ea0e5c
Anjali <amangla20@choate.edu>
2019-02-13 22:30:16 -0500

    dont know what im doing

--- worlds_hardest_gamefinal.py
+++ worlds_hardest_gamefinal.py
@@ -1,4 +1,5 @@
-import pygame
+# https://www.pygame.org/docs/tut/tom_games4.html
+import pygame, sys
 import math
 
 pygame.init()
@@ -9,41 +10,58 @@ balls = []
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("World's Hardest Game! Can you beat it in time?")
 done = False
-def oscillate_vertical(posx, posy):
-	for i in range(height - posy):
-		ball = Ball(posx, posy)
-		posy += 1
-class Ball:
-	def __init__(self, posx, posy):
-		self.posx = posx
-		self.posy = posy
-		pygame.draw.circle(screen, (0, 0, 255), [posx, posy], 5)
+time = pygame.time.get_ticks
+# def oscillate_vertical(posx, posy):
+# 	for i in range(height - posy):
+# 		ball = Ball(posx, posy)
+# 		posy += 1
 
-	def oscillate_vertical(self, posy):
-		for i in range(height - posy):
-			self.posy = posy
-			posy += 1
-			return posy
+# class Ball:
+# 	def __init__(self, posx, posy):
+# 		self.posx = posx
+# 		self.posy = posy
+# 		pygame.draw.circle(screen, (0, 0, 255), [posx, posy], 5)
+
+# 	def oscillate_vertical(self, posy):
+# 		newposy = self.posy + 1
+# 		self.__init__(self.posx, newposy)
+
+class Ball(pygame.sprite.Sprite):
+    """A ball that will move across the screen
+    Returns: ball object
+    Functions: update, calcnewpos
+    Attributes: area, vector"""
+
+    def __init__(self, posx, posy):
+        super().__init__()
+        pygame.draw.circle(screen, (0, 0, 255), [posx, posy], 5)
+
+    def oscillate_vertically(self, posy):
+    	if self.posy < height:
+    		self.posy += 1
+    	elif self.posy == height:
+    		self.posy -= 1
+    	elif self.posy == 0
+
 
 while not done:
 	# clock.tick(10)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-	pygame.display.flip()
 	#ball1 = Ball(100, 100)
+
 	posx = int(width/10)
 	posy = int(height/5)
+
 	for i in range(5):
 		balls.append(posx)
 		posx += 100
 
 	for i in range(5):
-		oscillate_vertical(balls[i], posy)
-
-
-
-
+		ball = Ball(balls[i], posy)
+		ball.oscillate_vertical(posy)
+		pygame.display.update()
 	pygame.display.flip()
 pygame.quit()
 # hints with text box for each level 
