b17c4f1ecee3aef39a0ae08895c7c7144a04d09c
Anjali <amangla20@choate.edu>
2019-02-22 11:30:39 -0500

    start screen not working

--- WorldsHardestGame/worldshardest.py
+++ WorldsHardestGame/worldshardest.py
@@ -11,6 +11,50 @@ Clock = pygame.time.Clock()
 #print(pygame.font.get_fonts())
 # instruct_screen()
 surface = pygame.display.set_mode((677,446))
+def loadBackground():
+	surface.blit(background, rect)
+	wall.display()
+	player = Player(surface, background, 75, 373, (255,0,0),15,15)
+	print(player.color)
+	# surface.blit(background, player.image, player.image)
+	# load_balls()
+	for ball in balls:
+		surface.blit(background, ball.image, ball.image)
+		ball.oscillate_direction()
+def text_display(message, posx, posy, color):
+	text_font = pygame.font.SysFont("comicsansms", 10)
+	text = text_font.render(message, True, color)
+	text_rect = text.get_rect()
+	text_rect.center = (posx, posy)
+	surface.blit(text, text_rect)
+def button(message, x,y,w,h,inactive_c,active_c, action=None):
+		mouse = pygame.mouse.get_pos()
+		click = pygame.mouse.get_pressed()
+		if x+w > mouse[0] > x and y+h > mouse[1] > y:
+			pygame.draw.rect(surface, active_c,(x,y,w,h))
+			if click[0] == 1 and action != None:
+				action()     
+		else:
+			pygame.draw.rect(surface, inactive_c,(x,y,w,h))
+
+		btn_font = pygame.font.SysFont("comicsansms",20)
+		btn = btn_font.render(message, True, (0, 0, 0))
+		btn_rect = btn.get_rect()
+		btn_rect.center = (x + (w/2), (y + (h/2)))
+		surface.blit(btn, btn_rect)
+def instruct_screen():
+	surface.fill((255, 255, 255))
+	text_display("HOW TO PLAY", 677/2, 446/2, (0, 0, 0))
+	button("CONTINUE (IF YOU DARE)", 677/2, 446 * 2/3, 200, 50, (255, 0, 0), (180, 0, 0), start_screen)
+
+def start_screen():
+	surface.fill((255, 255, 255))
+	text_display("WELCOME TO THE WORLD'S HARDEST GAME! CLICK START TO BEGIN, THOUGH I THINK IT'S FUNNY YOU THINK YOU CAN BEAT ME.", 677/2, 446/2, (0, 0, 0))
+	button("START!", 677/2, 446 * 2/3, 200, 50, 200, 50, (0, 255, 0), (0, 180, 0), loadBackground)
+
+
+instruct_screen()
+start_screen()
 death = 0
 coins = 0
 done = False
@@ -43,15 +87,6 @@ player = Player(surface, background, 75, 373, (255,0,0),15,15)
 # player.rect.y = 0
 #all_sprites_list.add(player)
 
-def loadBackground():
-	surface.blit(background, rect)
-	wall.display()
-	player = Player(surface, background, 75, 373, (255,0,0),15,15)
-	surface.blit(background, player.image, player.image)
-	# load_balls()
-	for ball in balls:
-		surface.blit(background, ball.image, ball.image)
-		ball.oscillate_direction()
 
 
 def collision_detection():
@@ -73,6 +108,9 @@ def collision_detection():
 			elif ball.kind == 3:
 				coins += 1
 				#balls.remove(ball)
+	for barrier in wall.barriers:
+		if barrier.getKind() == "2":
+			pass
 
 # def wall_collision():
 # 	for barrier in wall.barriers:
@@ -80,37 +118,6 @@ def collision_detection():
 # 		if barrier.getKind() == 1:
 # 			if int(barrier.posx) == player.posx:
 # 				noMoveRight = True
-
-def text_display(message, posx, posy, color):
-	text_font = pygame.font.SysFont("comicsansms", 10)
-	text = text_font.render(message, True, color)
-	text_rect = text.get_rect()
-	text_rect.center = (posx, posy)
-	surface.blit(text, text_rect)
-def button(message, x,y,w,h,inactive_c,active_c, action=None):
-		mouse = pygame.mouse.get_pos()
-		click = pygame.mouse.get_pressed()
-		if x+w > mouse[0] > x and y+h > mouse[1] > y:
-			pygame.draw.rect(surface, active_c,(x,y,w,h))
-			if click[0] == 1 and action != None:
-				action()     
-		else:
-			pygame.draw.rect(surface, inactive_c,(x,y,w,h))
-
-		btn_font = pygame.font.SysFont("comicsansms",20)
-		btn = btn_font.render(message, True, BLACK)
-		btn_rect = btn.get_rect()
-		btn_rect.center = (x + (w/2), (y + (h/2)))
-		surface.blit(btn, btn_rect)
-def instruct_screen():
-	surface.fill((255, 255, 255))
-	text_display("HOW TO PLAY", 677/2, 446/2, (0, 0, 0))
-	button("CONTINUE (IF YOU DARE)", 677/2, 446 * 2/3, 200, 50, (255, 0, 0), (180, 0, 0), start_screen)
-
-def start_screen():
-	surface.fill((255, 255, 255))
-	text_display("WELCOME TO THE WORLD'S HARDEST GAME! CLICK START TO BEGIN, THOUGH I THINK IT'S FUNNY YOU THINK YOU CAN BEAT ME.", 677/2, 446/2, (0, 0, 0))
-	button("START!", 677/2, 446 * 2/3, 200, 50, 200, 50, (0, 255, 0), (0, 180, 0), loadBackground)
 
 while not done:
 	print(death)
